c4e1860864c5f5b94f616a220816362d8f8155e6
Olona Mashalaba <166542412+OlonaMashalaba@users.noreply.github.com>
2024-11-01 19:17:39 +0200

    Add files via upload
    Added Docstrings

---
+++ subtrate.py
@@ -0,0 +1,10 @@
+"""
+This module performs subtraction operations.
+
+Functions:
+    subtract(a, b): Returns the result of subtracting b from a.
+"""
+
+def subtract(a, b):
+    """Returns the result of subtracting b from a."""
+    return a - b
